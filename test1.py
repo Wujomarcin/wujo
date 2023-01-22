5c87166dd7e31eae1781228b8ab7fdab7020251c
Wujomarcin <marcinwalencik1998@o2.pl>
2023-01-22 22:08:21 +0100

    i add wall number in outside

--- test1.py
+++ test1.py
@@ -14,3 +14,15 @@ for w in walls:
     if psets.get("Pset_WallCommon"):
         if bool(psets.get("Pset_WallCommon").get("IsExternal")):
             ext_walls.append(w)
+
+print(f'Liczba ścian zewnętrznych: {len(ext_walls)}')
+
+totalvolume = 0
+
+for w in ext_walls:
+    psets = ifcopenshell.util.element.get_psets(w)
+    for psetname, pset_dict in psets.items():
+        for name, value in pset_dict.items():
+            # print (f"{name}: {value}")
+            if name == "NetVolume":
+                totalvolume += float(value)           
